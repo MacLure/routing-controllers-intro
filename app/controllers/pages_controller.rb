1edb0f203fdc315a546d14c1224295fc81d9be06
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2018-12-04 13:45:43 -0500

    add secrets page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,36 @@
 class PagesController < ApplicationController
+    before_action :set_kitten_url, only: [:kitten, :kittens]
 
     def welcome
         @header = "This is the welcome page"
     end
 
     def contest
-        @header = "This is the Contest page"
-        render :contest
+        flash[:notice] = "Sorry, the contest has ended"
+        redirect_to "/welcome"
     end
 
     def kitten
+    end
+
+    def kittens
+    end
+
+    def set_kitten_url
         requested_size = params[:size]
         @kitten_url = "http://placekitten.com/#{requested_size}/#{requested_size}"
+    end
+
+    def secrets
+
+        if params[:magic_word] == "asdf"
+            render :secrets
+        else
+            flash[:alert] = "Sorry, you're not authorized to see that page!"
+            redirect_to "/welcome"
+        end
+
+
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'contest' => 'pages#contest'
 
   get '/kitten/:size' => 'pages#kitten'
+  get '/kittens/:size' => 'pages#kittens'
 
+  get '/secrets/:magic_word' => 'pages#secrets'
 
 end
